ee66217d45514d3cac9e969c2fd1ba2faf2bb8ff
Alex <alexridge2309@gmail.com>
2022-10-31 13:27:36 +0000

    directory update

--- directory.rb
+++ directory.rb
@@ -1,6 +1,32 @@
-puts "-" * 10 
-students = ["Dr. Hannibal Lecter", "Darth Vader", "Nurse Ratched",
-"Michael Corleone","Alex DeLarge", "The Wicked Wicked of the West",
-"Terminator", "Freddy Kruger", "The Joker", "Joffrey Baratheon",
-"Norman Bates"]
-puts students
+students = [
+  ["Dr. Hannibal Lecter", :november],
+  ["Darth Vader", :november],
+  ["Nurse Ratched", :november],
+  ["Michael Corleone", :november],
+  ["Alex DeLarge", :november],
+  ["The Wicked Wicked of the West", :november],
+  ["Terminator", :november], 
+  ["Freddy Kruger", :november],
+  ["The Joker", :november],
+  ["Joffrey Baratheon", :november],
+  ["Norman Bates", :november]
+]
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-" * 10 
+end 
+
+def print(names)
+    names.each do |student|
+      puts "#{student[0]} (#{student[1]} cohort)"
+    end 
+end 
+
+def print_footer(names)
+  puts "overall we have #{names.length} great students."
+end 
+
+print_header
+print(students)
+print_footer(students)
