55862a6e21850ace40ebc9ddd62c62a2d1922fcf
Alex <alexridge2309@gmail.com>
2022-10-31 13:09:18 +0000

    directory.rb

---
+++ directory.rb
@@ -0,0 +1,6 @@
+puts "-" * 10 
+students = ["Dr. Hannibal Lecter", "Darth Vader", "Nurse Ratched",
+"Michael Corleone","Alex DeLarge", "The Wicked Wicked of the West",
+"Terminator", "Freddy Kruger", "The Joker", "Joffrey Baratheon",
+"Norman Bates"]
+puts students
